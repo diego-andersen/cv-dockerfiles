7fa58bd4336909dc2f97bcc6781d094fa32ce348
Diego Andersen <diego.andersen@protonmail.com>
2023-04-02 14:05:29 +0200

    Add (soon-to-be) cross-platform dependency installation and build scripts for OpenCV.

---
+++ provisioning/build_opencv.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+set -e
+
+usage()
+{
+cat << EOF
+Usage: $0 [--gpu|--gstreamer|--contrib]
+
+Build OpenCV from source and install it to /usr/local.
+
+OPTIONS:
+    -h|--help    Print help and exit
+    -j|--jobs    Number of make jobs (same as make -j)
+    --gpu        Include GPU support (CUDA)
+    --gstreamer  Include gstreamer support
+    --contrib    Include opencv-contrib
+EOF
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --gpu) add_gpu=1 ;;
+        --gstreamer) add_gstreamer=1 ;;
+        --contrib) add_contrib=1 ;;
+        -j|--jobs) n_make_jobs=$2; shift ;;
+        -h|--help) usage; exit ;;
+        *) echo "Unknown parameter: $1"; usage; exit 1 ;;
+    esac
+    shift
+done
+
+git clone --depth 1 --b $OPENCV_VERSION https://github.com/opencv/opencv.git
+
+# Ignore spaces inside array values
+IFS=""
+
+cmake_flags=(
+    # General config
+    "-D CMAKE_BUILD_TYPE=RELEASE"
+    "-D CMAKE_INSTALL_PREFIX=/usr/local"
+    "-D OPENCV_GENERATE_PKGCONFIG=ON"
+    "-D CPACK_BINARY_DEB=ON"
+    "-D BUILD_PERF_TESTS=OFF"
+    "-D BUILD_TESTS=OFF"
+    "-D BUILD_EXAMPLES=OFF"
+    "-D INSTALL_PYTHON_EXAMPLES=OFF"
+    "-D INSTALL_C_EXAMPLES=OFF"
+    "-D BUILD_OPENCV_JAVA=OFF"
+
+    # Python config
+    "-D BUILD_opencv_python2=OFF"
+    "-D BUILD_opencv_python3=ON"
+    "-D PYTHON3_EXECUTABLE=$(command -v python)"
+    "-D PYTHON3_INCLUDE_DIR=$(python -c 'from distutils.sysconfig import get_python_inc; print(get_python_inc())')"
+    "-D PYTHON3_PACKAGES_PATH=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')"
+
+    # Optional modules
+    "-D WITH_EIGEN=ON"
+    "-D EIGEN_INCLUDE_PATH=/usr/include/eigen3"
+    "-D ENABLE_NEON=OFF"
+    "-D WITH_GSTREAMER=ON"
+    "-D WITH_LIBV4L=ON"
+    "-D WITH_OPENGL=ON"
+    "-D WITH_OPENCL=OFF"
+    "-D WITH_IPP=OFF"
+    "-D WITH_TBB=ON"
+    "-D WITH_QT=OFf"
+    "-D BUILD_TIFF=ON"
+)
+
+# Check for CUDA
+if [ -v add_gpu ]
+then
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D WITH_CUDA=ON"
+)
+else
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D WITH_CUDA=OFF"
+)
+fi
+
+# Check for gstreamer
+if [ -v add_gstreamer ]
+then
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D WITH_GSTREAMER=ON"
+)
+else
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D WITH_GSTREAMER=OFF"
+)
+fi
+
+# Check for non-free modules
+if [ -v add_contrib ]
+then
+git clone --depth 1 --b ${OPENCV_VERSION} https://github.com/opencv/opencv_contrib.git
+
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D OPENCV_ENABLE_NONFREE=ON"
+    "-D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules"
+)
+else
+cmake_flags=(
+    ${cmake_flags[@]}
+    "-D OPENCV_ENABLE_NONFREE=OFF"
+)
+fi
+
+cd opencv
+mkdir build
+cd build
+
+cmake "${cmake_flags[@]}" ..
+make -j ${n_make_jobs:-8}
+make install
+ldconfig
---
+++ provisioning/install_dependencies.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+set -e
+
+readonly linux_pkgs=(
+    python3
+    python3-dev
+    python3-pip
+    python3-venv
+    build-essential
+    cmake
+    git
+    udev
+)
+
+readonly ubuntu_pkgs=(
+    ${linux_pkgs[@]}
+    # gfortran
+    libatlas-base-dev
+    libavcodec-dev
+    libavformat-dev
+    libavresample-dev
+    libcanberra-gtk3-module
+    libdc1394-22-dev
+    libeigen3-dev
+    libglew-dev
+    libgstreamer1.0-dev
+    libgstreamer-plugins-base1.0-dev
+    libgstreamer-plugins-good1.0-dev
+    libgtk-3-dev
+    libjpeg-dev
+    libjpeg8-dev
+    libjpeg-turbo8-dev
+    liblapack-dev
+    liblapacke-dev
+    libopenblas-dev
+    libpng-dev
+    libpostproc-dev
+    libswscale-dev
+    libtbb-dev
+    libtbb2
+    libtesseract-dev
+    libtiff-dev
+    libv4l-dev
+    libxine2-dev
+    libxvidcore-dev
+    libx264-dev
+    libgtkglext1
+    libgtkglext1-dev
+    pkg-config
+    qv4l2
+    v4l-utils
+    v4l2ucp
+    zlib1g-dev
+)
+
+readonly ubuntu1804_pkgs=(
+    ${ubuntu_pkgs[@]}
+    python3.8
+    python3.8-dev
+    python3.8-venv
+)
+
+# Docker version doesn't require sudo
+if [[ -f /.dockerenv ]] || grep -Eq '(lxc|docker|containerd)' /proc/1/cgroup; then
+    echo "Installing Docker packages"
+    source /etc/os-release
+
+    case "$ID" in
+    ubuntu)
+        apt purge libopencv*
+        apt update
+
+        if [[ $VERSION_ID == "20.04" ]]; then
+            echo -e "Installing Ubuntu 20.04 packages\n"
+            apt install -y --no-install-recommends "${ubuntu_pkgs[@]}"
+            python3 -m pip install -U pip setuptools wheel
+        elif [[ $VERSION_ID == "18.04" ]]; then
+            echo -e "Installing Ubuntu 18.04 packages\n"
+             apt install -y --no-install-recommends "${ubuntu1804_pkgs[@]}"
+            python3.8 -m pip install -U pip setuptools wheel
+        fi
+        ;;
+    *)
+        echo "ERROR: Distribution not supported"
+        exit 99
+        ;;
+    esac
+elif [[ ! $(uname -m) =~ ^arm* && -f /etc/os-release ]]; then
+    echo -e "Installing desktop packages\n"
+    source /etc/os-release
+
+    case "$ID" in
+    ubuntu|debian)
+        sudo apt purge libopencv-dev libopencv-python libopencv-samples libopencv*
+
+        if [[ $VERSION_ID == "20.04" ]]; then
+            echo -e "Installing Ubuntu 20.04 packages\n"
+            sudo apt install -y --no-install-recommends "${ubuntu_pkgs[@]}"
+            python3 -m pip install -U pip setuptools wheel
+        elif [[ $VERSION_ID == "18.04" ]]; then
+            echo -e "Installing Ubuntu 18.04 packages\n"
+            sudo apt install -y --no-install-recommends "${ubuntu1804_pkgs[@]}"
+            python3.8 -m pip install -U pip setuptools wheel
+        fi
+        ;;
+    *)
+        echo "ERROR: Distribution not supported"
+        exit 99
+        ;;
+    esac
+else
+    echo "ERROR: Host not supported"
+    exit 99
+fi
